b721740a54a11ad69fb64c8f51171b159474c49d
Breynner Ariza <breynner.fabian.ariza.florez@gmail.com>
2024-12-17 23:47:04 -0500

    Configuration gateway service - finished lab

---
+++ repaso-course/person-gateway/src/main/java/com/server/gateway/person/PersonGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.server.gateway.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonGatewayApplication.class, args);
+	}
+
+}
